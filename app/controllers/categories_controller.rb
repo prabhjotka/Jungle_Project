6af070836a6bd8cbb44ca1558b7a82cdcffa321c
Prabh <sonukaur68@gmail.com>
2023-11-16 14:30:12 -0700

    admin-categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    @category = @category.products.order(created_at: :desc)
   end
 
 end
