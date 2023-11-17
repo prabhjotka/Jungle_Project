1b22d4314895d2e61fb2b54ce536c84abf3c7faa
Prabh <sonukaur68@gmail.com>
2023-11-16 17:50:10 -0700

    fix functionality in category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
-    @category = @category.products.order(created_at: :desc)
+    @products = @category.products.order(created_at: :desc)
   end
 
 end
